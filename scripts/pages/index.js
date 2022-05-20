3357e0236ff1f9b2c2e8a58202182a2d42bba508
bbeatrice <techerbaretbeatrice@gmail.com>
2022-05-20 13:37:26 +0200

    init photographers and photographer pages

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,45 +1,71 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+const displayPhotographerList = () => {
+    const data = fetchData()
+    const photographers = data.photographers
+    for (let photographer of photographers) {
+        const card = Card(photographer)
+        document.getElementById('main').appendChild(card)
     }
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+}
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+// Ne pas se focus sur cette methode
+const fetchData = () => {
+    return window.DATA
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+// cree une boite qui va contenir la card d'un photographe
+// on lui passe un parametre photographe
+const Card = (photographer) => {
+    const card = document.createElement('div')
+    card.setAttribute("class","photographer_card")
+    const link = Link(photographer)
+    const photo = Photo(photographer)
+    const title = Title(photographer)
+    const details = Details(photographer)
+//
+    link.appendChild(photo)
+    link.appendChild(title)
+    card.appendChild(link)
+    card.appendChild(details)
+
+    return card;
+}
+
+const Photo = (photographer) => {
+    const photo = document.createElement('img')
+    photo.setAttribute("class","photographer_img")
+    console.info(photographer)
+    // creer un lien qui contient assets/photographers/PORTRAIT
+    photo.src = `assets/photographers/${photographer.portrait}`
+    return photo;
+}
+
+const Link = (photographer) => {
+    const link = document.createElement('a')
+    link.href = `photographer.html?photographer=${photographer.id}`
+    return link;
+}
+
+const Title = (photographer) => {
+    const title = document.createElement('h2')
+    title.setAttribute("class", "photographer_name")
+    title.textContent = photographer.name
+    return title;
+}
+
+const Details = (photographer) => {
+    const details = document.createElement('div')
+    details.setAttribute("class", "photographer_info")
+    details.innerHTML =`
+    <p class="photographer_city">${photographer.city}
+    </p>
+    <br>
+    <p>${photographer.tagline}</p>
+    <br>
+    <p>${photographer.price}€/jour</p>
+`
+    return details;
+}
+
+
+displayPhotographerList()
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,138 @@
-//Mettre le code JavaScript lié à la page photographer.html
+//on récupère l'id de chaque photographe dans l'url de la page phototographe
+const params = (new URL(document.location)).searchParams;
+const photographerId = params.get('photographer'); 
+console.info(photographerId)
+
+//
+const displayPhotographer = () => {
+    const data = fetchData()
+    const photographers = data.photographers // utiliser find()
+    const photographer = photographers.find(photographer => photographer.id === Number(photographerId))
+    console.log(photographer)
+    const infoCard = InfoCard(photographer)
+    document.getElementById('photographer_main').appendChild(infoCard)
+    console.log(infoCard)
+}
+
+// Ne pas se focus sur cette methode
+const fetchData = () => {
+    return window.DATA
+}
+//on créer la fonction qui permet de retourner la card info de la page photographe
+//qui contient une descrition, un bouton contact et la photo du photographe
+const InfoCard = (photographer) => {
+    const infoCard = document.createElement('section')
+    infoCard.setAttribute("class","photographer_infos")
+    console.log(infoCard)
+    const infoBox = document.createElement('div')
+    infoBox.setAttribute("class","info_box")
+    const title = Title(photographer)
+    const details = Details(photographer)
+    infoBox.appendChild(title)
+    infoBox.appendChild(details)
+    const contactButton = ContactButton(photographer)
+    const photo = Photo(photographer)
+    
+    infoCard.appendChild(infoBox)
+    infoCard.appendChild(contactButton)
+    infoCard.appendChild(photo)
+    console.log(infoCard)
+    return infoCard
+   
+}
+//permet de retourner le nom 
+const Title = (photographer) =>{
+    const title = document.createElement('h2')
+    title.setAttribute("class", "photographer_name")
+    title.textContent = photographer.name
+    console.log(title)
+    return title;
+    
+}
+//permet de retourner les infos ville ...
+const Details = (photographer) =>{
+    const details = document.createElement('div')
+    details.setAttribute("class", "photographer_info")
+    details.innerHTML = `
+    <p class="photographer_city">${photographer.city}
+    </p>
+    <br>
+    <p>${photographer.tagline}</p> 
+`
+    return details;   
+}
+
+//permet de retourner le bouton contact
+const ContactButton = (photographer) =>{
+    const contactButton = document.createElement('button')
+    contactButton.setAttribute("class" , "contact_button")
+    contactButton.textContent = "Contactez-moi"
+
+    return contactButton
+}
+
+//permet de retourner la photo
+const Photo = (photographer) => {
+    const photo = document.createElement('img')
+    photo.setAttribute("class","photographer_img")
+    console.info(photographer)
+    photo.src = `assets/photographers/${photographer.portrait}`
+    return photo;
+}
+
+
+displayPhotographer()
+
+//créer en utilisant la méthode filter une fonction qui récupère dans les média les photos selon l'id du photographe 
+//afin de pouvoir afficher sa liste de photos
+const displayMedia = () =>{
+    const data = fetchData()  
+    const media = data.media
+    const medias = media.filter(medias => medias.photographerId === Number(photographerId) )
+    console.log(medias)
+    for(let media of medias){
+        const mediaBox = MediaBox(media)
+    document.getElementById('photographer_main').appendChild(mediaBox)
+    console.log(mediaBox)
+    }
+    
+}
+
+const MediaBox = (media) =>{
+    const mediaBox = document.createElement('section')
+    mediaBox.setAttribute("class", "media_box")
+    const photoBox = document.createElement('div')
+    photoBox.setAttribute("class", "photo_box")
+    const photoParameters = document.createElement('div')
+    photoParameters.setAttribute("class","photo_parameters")
+    const photoMedia = PhotoMedia(media)
+    const photoDescription = PhotoDescription(media)
+    const numberOfLike = NumberOfLike(media)
+    
+    mediaBox.appendChild(photoBox)
+    mediaBox.appendChild(photoParameters)
+    photoBox.appendChild(photoMedia)
+   // photoParameters.appendChild(photoDescription)
+    //photoParameters.appendChild(numberOfLike)
+    
+    return mediaBox
+}
+
+const PhotoMedia = (media) =>{
+  const photoMedia = document.createElement('img')
+  photoMedia.setAttribute("class", "photo_media")
+  photoMedia.src = `assets/images/${media.photographerId}/${media.image}`
+
+  return photoMedia
+}
+
+const PhotoDescription = (media) =>{
+
+}
+
+const NumberOfLike = (media) =>{
+
+}
+
+displayMedia()
+
